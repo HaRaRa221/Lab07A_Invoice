478846dbe97da80da3ccfae8d82e12c077bbac7e
Raymond Ha <Haro@mail.uc.edu>
2023-10-30 15:42:29 -0400

    Initial

--- src/LineItem.java
+++ src/LineItem.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class LineItem {
+public class LineItem {
+    private Product product;
+    private int quantity;
+
+    public LineItem (Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public double getTotal() {
+        return this.product.getPriceUnit() * this.quantity;
+    }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Product {
+public class Product {
+    private String name;
+    private double priceUnit;
+
+    public Product(String name, double priceUnit) {
+        this.name = name;
+        this.priceUnit = priceUnit;
+    }
+    public String getName() {
+        return this.getName();
+    }
+    public double getPriceUnit() {
+        return this.priceUnit;
+    }
 }
